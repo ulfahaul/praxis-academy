8bb1c4f5200a09151d25631fcbb5c9fd0a6b5336
ulfahaul <ulfahaulia.2017@student.uny.ac.id>
2022-06-01 15:43:46 +0700

    jinja

---
+++ enterprise-python/minggu-3/hari-3/flask_example/run.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+    return render_template(
+        'template.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="Home")
+
+@app.route("/about")
+def about():
+    return render_template(
+        'template.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="About")
+
+@app.route("/contact")
+def contact():
+    return render_template(
+        'template.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="Contact Us")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
